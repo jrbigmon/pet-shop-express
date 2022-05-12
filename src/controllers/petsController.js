3d88f1dc2c2e62090bb3153988e3876c4ba42c23
jrbigmon <junioterriaga0@gmail.com>
2022-05-12 17:38:15 -0300

    Adicionado todos os métodos http para o petsController e petsRouter

--- src/controllers/petsController.js
+++ src/controllers/petsController.js
@@ -16,7 +16,21 @@ const petsController = {
         const {name, petType, age, size} = req.body;
         petsDatabase.push({id: petsDatabase.length+1, name, petType, age, size})
         res.send(petsDatabase)
+    },
+    petsUpdated: (req, res) => {
+        const {id} = req.params;
+        let index = petsDatabase.find(element => element.id == id)
+        const {name, petType, age, size} = req.body;
+        petsDatabase[index] = {name, petType, age, size}
+        res.send(petsDatabase[index])
+    },
+    petsDeleted: (req, res) => {
+        const {id} = req.body;
+        const index = petsDatabase.find(element => element.id == id)
+        petsDatabase.splice(index, 1)
+        res.send(petsDatabase)
     }
+
 
 }
 
--- src/routes/petsRouter.js
+++ src/routes/petsRouter.js
@@ -5,6 +5,6 @@ const petsController = require('../controllers/petsController')
 router.get('/pets', petsController.petsList)
 router.get('/pets/:id', petsController.petIndex)
 router.post('/pets/create', petsController.petsCreated)
-
-
+router.put('/pets/update/:id', petsController.petsUpdated)
+router.delete('/pets/delete', petsController.petsDeleted)
 module.exports = router

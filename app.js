01547bd3bf31aa2294f0b3f13a0f6a221431e2cf
jrbigmon <junioterriaga0@gmail.com>
2022-05-13 21:06:45 -0300

    modificado o metodo usersCreated e petsCreated para gerar id com o uuid module

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
-// app.set('views', __dirname + '/views')
-// app.set("views", path.join(__dirname, "/views"));
-app.use(express.static(__dirname + 'public'));  
+app.set('views', path.resolve('src', 'views'))
+app.use(express.static('public'));  
 
 app.use('/', userRouter);
 app.use('/', petsRouter);

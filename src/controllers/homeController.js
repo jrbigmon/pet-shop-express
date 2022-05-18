81071abead2eec1edc78163b2ee03907466c28ad
jrbigmon <junioterriaga0@gmail.com>
2022-05-18 20:44:54 -0300

    Adicionado nova pasta chamda home e movido os arquivos.ejs da home para essa pasta e modificado para a rota correta

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,15 @@
 const homeController = {
     index: (req, res) => {
-        res.render('index', {title: 'Home'})
+        res.render('./home/index', {title: 'Home'})
     },
     service: (req, res) => {
-        res.render('service', {title: 'service'})
+        res.render('./home/service', {title: 'service'})
     },
     login: (req, res) => {
-        res.render('login', {title: 'login'})
+        res.render('./home/login', {title: 'login'})
     },
     contact: (req, res) => {
-        res.render('contact', {title: 'contact'})
+        res.render('./home/contact', {title: 'contact'})
     }
 }
 module.exports = homeController;

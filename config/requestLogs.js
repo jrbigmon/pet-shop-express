ea4c869daebd1f41db416d14d210e86995957e8e
jrbigmon <junioterriaga0@gmail.com>
2022-06-06 20:26:06 -0300

    alterado método de busca de ip e adicionado mais um \n para pular linha

--- config/requestLogs.js
+++ config/requestLogs.js
@@ -4,13 +4,12 @@ const moment = require('moment')
 const requestLogs = (req, res, next) => {
     let router = req.url;
     let method = req.method;
-    let ip = req.connection.remoteAddress;
-    let status;
-    req.status == undefined ? status = '200 OK' : status = req.status;
+    let ip = req.ip;
+    let status = req.status == undefined ? status = '200 OK' : status = req.status;
     let date = moment().format('DD MM YYYY HH:mm:ss');
 
-    let data = `Rota: ${router}, Metodo HTTP: ${method}, IP: ${ip}, Status Servidor: ${status}, Data: ${date} \n`
+    let data = `Rota: ${router}, Metodo HTTP: ${method}, IP: ${ip}, Status Servidor: ${status}, Data: ${date} \n\n`
     fs.appendFileSync('clientLogs.txt', data, 'utf8')
-    next();
+    return next();
 }
 module.exports = requestLogs;

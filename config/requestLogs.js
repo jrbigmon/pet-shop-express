414a195f90ebdc77cbfe8b9a393fc2e333ad9d7c
jrbigmon <junioterriaga0@gmail.com>
2022-06-06 20:39:40 -0300

    colocado clientLogs.txt para .gitignore para não deixar o sistema sobrecarregado

--- config/requestLogs.js
+++ config/requestLogs.js
@@ -2,13 +2,10 @@ const fs = require('fs')
 const moment = require('moment')
 
 const requestLogs = (req, res, next) => {
-    let router = req.url;
-    let method = req.method;
-    let ip = req.ip;
-    let status = req.status == undefined ? status = '200 OK' : status = req.status;
     let date = moment().format('DD MM YYYY HH:mm:ss');
 
-    let data = `Rota: ${router}, Metodo HTTP: ${method}, IP: ${ip}, Status Servidor: ${status}, Data: ${date} \n\n`
+    let data = `Rota: ${req.url}, Metodo HTTP: ${ req.method}, IP: ${req.ip}, Status Servidor: ${res.statusCode}, Data: ${date} \n\n`
+    
     fs.appendFileSync('clientLogs.txt', data, 'utf8')
     return next();
 }

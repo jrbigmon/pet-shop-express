cba3d391411e63851265107109eaefa83188530b
jrbigmon <junioterriaga0@gmail.com>
2022-06-15 08:57:03 -0300

    enviando res.locals para o headerteste, com o intuito de fazer uma validação e apresentação de acordo com o user logado

--- config/sessionLoggedIn.js
+++ config/sessionLoggedIn.js
@@ -1,7 +1,7 @@
 const userLoggedIn = (req, res, next) => {
     const userAproved = req.session.userLoggedIn ? req.session.userLoggedIn: false
     if(userAproved.email === 'junior@gmail.com') {
-        res.locals.user = userAproved;
+        res.locals.user = req.session.userLoggedIn;
         return next();
     }else {
         res.redirect('/login');
